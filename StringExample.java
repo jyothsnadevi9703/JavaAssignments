31318a76731b2f7e8b4c5461bd00758245c30977
Jyothsna Devi <104260689+jyothsnadevi9703@users.noreply.github.com>
2022-11-20 18:01:00 +0530

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,25 @@
+package StringExample;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num[]= {1,2,3,4,5};
+		String value[]= {"java","programming","is","popular","language"};
+		char data[]= {'a','9','#','*','3'};
+		
+//		System.out.println(num[0]);
+//		System.out.println(value[1]);
+//		System.out.println(data[2]);
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(num[i]);
+//			System.out.println(value[i]);
+//			System.out.println(data[i]);
+		}
+		
+
+	}
+
+}
