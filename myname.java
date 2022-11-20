8af4f08cffeb20918f208295cfe170b73dbd09dc
Jyothsna Devi <104260689+jyothsnadevi9703@users.noreply.github.com>
2022-11-20 18:05:08 +0530

    Add files via upload
    InputFromUser

---
+++ myname.java
@@ -0,0 +1,17 @@
+package inputFromUser;
+
+import java.util.Scanner;
+
+public class myname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("What is your name?");
+		Scanner n=new Scanner(System.in);
+		String name=n.next();
+		System.out.println(name);
+
+	}
+
+}
