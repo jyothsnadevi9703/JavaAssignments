d026afbccbcdd3a336952ff3b3d722dd603fdca4
Jyothsna Devi <104260689+jyothsnadevi9703@users.noreply.github.com>
2022-11-21 12:06:19 +0530

    Update MarksPercentage.java

--- MarksPercentage.java
+++ MarksPercentage.java
@@ -9,6 +9,7 @@ public class MarksPercentage {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the length of subjects");
 		int n=sc.nextInt();
+		int totalMarks=n*100;
 		int[]subject=new int[n];
 		System.out.println("Enter the Marks");
 		for (int i=0;i<n;i++)
@@ -21,13 +22,15 @@ public class MarksPercentage {
 			System.out.println(subject[i]);
 		}
 		int sum=0;
+		float percentage;
 		for (int i=0;i<n;i++)
 		{
 			sum=sum+subject[i];
 		}
-		int percentage = ((sum/n*100)*100);
+		percentage=((sum*100)/totalMarks);
 //		double percentage[]=new double[(sum/n*100)*100];
-		System.out.println("Percentage of Marks- "+ percentage);
+		System.out.println("sum of the marks- "+sum);
+		System.out.println("Percentage of Marks- "+percentage+"%");
 		System.out.print("Average of Marks- "+sum/n);
 
 	}
