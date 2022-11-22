1e96d0484b7581f80570cf70468f33669617bdad
Jyothsna Devi <104260689+jyothsnadevi9703@users.noreply.github.com>
2022-11-22 23:48:29 +0530

    Add files via upload

---
+++ Assignment_1.java
@@ -0,0 +1,23 @@
+package Assignment_1;
+
+import java.util.Scanner;
+
+public class Assignment_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		int principle;
+		int interest;
+		int time;
+		System.out.println("Enter principle, interest, time :");
+		Scanner sc=new Scanner(System.in);
+		principle=sc.nextInt();
+		interest=sc.nextInt();
+		time=sc.nextInt();
+		int si=(principle*interest*time)/100;
+		System.out.println("Simple Interest is: " +si);
+	}
+}
+
+	
+
