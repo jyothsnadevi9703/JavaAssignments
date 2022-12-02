2a03025f0266cb9c26cb1dfaf874af11d3a91209
Jyothsna Devi <104260689+jyothsnadevi9703@users.noreply.github.com>
2022-12-02 10:50:53 +0530

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,27 @@
+package ArrayList;
+
+public class Myclass {
+	
+//	int number;
+//	Myclass()//default contructor
+//	{
+//		
+//	}
+//	Myclass(int num)
+//	{
+//		number=num;
+//	}
+//	public void printnum()
+//	{
+//		System.out.println(number);
+//	}
+	
+	
+	
+	int number=10;
+	public void returnnum()
+	{
+		System.out.println(number);
+	}
+
+}
