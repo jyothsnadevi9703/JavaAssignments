f9510f1f89f60e408c040bec9e873f19781bfb7e
Jyothsna Devi <104260689+jyothsnadevi9703@users.noreply.github.com>
2022-12-06 12:01:35 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int findMax(int arr[])
+    {
+        int max=arr[0];
+        for (int i=0;i<arr.length;i++)
+        {
+            if (max<arr[i])
+                max=arr[i];
+        }
+        return max;
+    }
+}
