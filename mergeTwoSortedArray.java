858dc1d29195ecf32cb0700d56b1a6f9ba6d283b
Jyothsna Devi <104260689+jyothsnadevi9703@users.noreply.github.com>
2022-12-24 09:54:53 +0530

    Add files via upload

---
+++ mergeTwoSortedArray.java
@@ -0,0 +1,29 @@
+package ArrayAssignment;
+
+public class mergeTwoSortedArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr1[]=new int [] {1,2,3,4,5};
+		int arr2[]=new int [] {6,7,8,9,10};
+		
+		int c=0;
+		 for(int i:arr1) {
+		   c++;
+		 }
+		
+		System.out.println( );
+//		for(int i=0;  ) {
+			
+//		}
+
+	}
+
+}
+
+
+
+//for i in b:
+//    if i not in a:
+//        a=a+[i]
+//print(a)
